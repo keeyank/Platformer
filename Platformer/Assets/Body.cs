6e145b730aa709e2fa40c4789bd2a0cde2f31794
Keeyan <kkazemi@sfu.ca>
2018-12-25 16:29:19 -0800

    Physics & Movement
    Created "Body" class, which has all physics and movement for any possible character in the game, including the player. Set up raycasting in this class to process collisions and ensure no intersections between a class inheriting body and any collidables will occur (not yet implemented fully)

---
+++ Platformer/Assets/Body.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : MonoBehaviour
+{
+
+    private static int gravity = 5;
+    protected int speed;
+
+    protected Body(int speed)
+    {
+        this.speed = speed;
+    }
+
+    // Move body towards a certain direction, by a certain value
+    protected void MoveBody(Vector3 direction, float speed)
+    {
+        Vector3 newPos = transform.position + (direction * speed * Time.deltaTime);
+
+        Vector3 ySize = new Vector3(0, GetComponent<BoxCollider2D>().size.y/2, 0);
+        Vector3 startPos = transform.position - ySize;
+        int layerMask = 1 << 8; // bit sequence 1000 0000 - Only 'collidable' layer
+
+        // Cast raycast and find a collider it hits
+        RaycastHit2D hit = Physics2D.Raycast(startPos, direction, speed * Time.deltaTime, layerMask);
+        Debug.DrawRay(startPos, direction * speed * Time.deltaTime, Color.yellow);
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name);    
+        }
+
+
+
+        // Update gameObject's transform.position here based on whether a collidable was found
+    }
+
+    protected virtual void Update()
+    {
+        MoveBody(Vector3.down, gravity);
+    }
+}
--- Platformer/Assets/Player.cs
+++ Platformer/Assets/Player.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Body
 {
-    // Start is called before the first frame update
-    void Start()
+    // speed = 2
+    Player() : base(2) { }
+
+    protected override void Update()
     {
+        base.Update();
+
+        // Move player each frame if keys are held down
+        if (Input.GetKey(KeyCode.D))
+        {
+            MoveBody(Vector3.right, speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            MoveBody(Vector3.left, speed);
+        }
         
+        // Check if collider for this object is intersecting with collider for another object
+        // If it is, use the saved position of the gameObject at the beginning of the frame (i.e., in the update function for Body
+        // and revert to this position (save the position each update at the beginning in the Body script, then revert to this position)
+
+        // Checking should be at the very end of the update (before the position is updated in the next frame, so the sprites never intersect)
+
     }
 
-    // Update is called once per frame
-    void Update()
+    //If your GameObject starts to collide with another GameObject with a Collider
+    void OnCollisionEnter(Collision collision)
     {
-        
+        //Output the Collider's GameObject's name
+        Debug.Log(collision.collider.name);
     }
+
 }
