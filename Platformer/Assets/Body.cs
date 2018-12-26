6b008387041850fd218fa996ff11fda5c55e6f71
Keeyan <kkazemi@sfu.ca>
2018-12-25 20:50:04 -0800

    Collision Detection Working
    Well, not really, but in most cases it's working. Implemented by shooting a raycast to the area the player wants to move to, and if that raycast collides with anything, move the player to the collision point.
    
    There's a bug when the player's centerpoint isn't lined up with a tile, and he will just fall through, since the raycast is shot from the center of the player. To fix, we can shoot 2 addition raycasts from the player's corners, but this still doesn't completely fix the bug (in case there are 2 small platforms between each raycast).

--- Platformer/Assets/Body.cs
+++ Platformer/Assets/Body.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Body : MonoBehaviour
 {
@@ -14,25 +15,59 @@ public class Body : MonoBehaviour
     }
 
     // Move body towards a certain direction, by a certain value
+    // Direction can only be up, down, left, or right
+    // Body must consist of a single Box Collider 2D
     protected void MoveBody(Vector3 direction, float speed)
     {
+        Assert.IsTrue(direction == Vector3.down || direction == Vector3.up
+            || direction == Vector3.left || direction == Vector3.right);
+
+        BoxCollider2D coll = GetComponent<BoxCollider2D>();
         Vector3 newPos = transform.position + (direction * speed * Time.deltaTime);
 
-        Vector3 ySize = new Vector3(0, GetComponent<BoxCollider2D>().size.y/2, 0);
-        Vector3 startPos = transform.position - ySize;
-        int layerMask = 1 << 8; // bit sequence 1000 0000 - Only 'collidable' layer
-
-        // Cast raycast and find a collider it hits
-        RaycastHit2D hit = Physics2D.Raycast(startPos, direction, speed * Time.deltaTime, layerMask);
-        Debug.DrawRay(startPos, direction * speed * Time.deltaTime, Color.yellow);
-        if (hit.collider != null)
+        // Define offset to add to raycast, since raycast begins at origin of collider
+        float offset;
+        if (direction == Vector3.left || direction == Vector3.right)
         {
-            Debug.Log(hit.collider.gameObject.name);    
+            offset = coll.size.x/2;
+        }
+        else // direction is up or down
+        {
+            offset = coll.size.y/2;
         }
 
+        // Layer mask ensures only collidable objects are found in raycast
+        int layerMask = 1 << 8; // bit sequence 1000 0000 - Only 'collidable' layer
 
+        // Cast raycast 
+        RaycastHit2D hit = 
+            Physics2D.Raycast(transform.position, direction, offset + speed * Time.deltaTime, layerMask);
+        Debug.DrawRay(transform.position, 
+            (direction * offset) +  (direction * speed * Time.deltaTime), Color.yellow);
 
-        // Update gameObject's transform.position here based on whether a collidable was found
+        // Snap newPos to edge of any colliders found
+        if (hit.collider != null)
+        {
+            if (direction == Vector3.left)
+            {
+                newPos = new Vector3(hit.point.x + coll.size.x / 2, newPos.y, newPos.z);
+            }
+            else if (direction == Vector3.right)
+            {
+                newPos = new Vector3(hit.point.x - coll.size.x / 2, newPos.y, newPos.z);
+            }
+            else if (direction == Vector3.down)
+            {
+                newPos = new Vector3(newPos.x, hit.point.y + coll.size.y / 2, newPos.z);
+            }
+            else if (direction == Vector3.up)
+            {
+                newPos = new Vector3(newPos.x, hit.point.y - coll.size.y / 2, newPos.z);
+            }
+        }
+
+        // Update the body's position
+        transform.position = newPos;
     }
 
     protected virtual void Update()
