dfd3e6c4d79abebe0327ebef1c48a8c7f6b57b6a
Keeyan <kkazemi@sfu.ca>
2018-12-27 14:23:28 -0800

    Tried implementing collisions differently
    Includes a few different attempts to implement it, still trying to figure out best way to do it

--- Platformer/Assets/Body.cs
+++ Platformer/Assets/Body.cs
@@ -3,31 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class Body : MonoBehaviour
-{
+public class Body : MonoBehaviour {
 
     private static int gravity = 5;
-    protected int speed;
+    protected float speed;
 
-    protected Body(int speed)
-    {
+    protected Body(float speed) {
         this.speed = speed;
     }
 
     // Move body towards a certain direction, by a certain value
     // Direction can only be up, down, left, or right
     // Body must consist of a single Box Collider 2D
-    protected void MoveBody(Vector3 direction, float speed)
-    {
-        Assert.IsTrue(direction == Vector3.down || direction == Vector3.up
-            || direction == Vector3.left || direction == Vector3.right);
+    // Assertion: "Collidable" tiles use a single BoxCollider2D
+    protected void MoveBody(Vector2 direction, float speed) {
+        Assert.IsTrue(direction == Vector2.down || direction == Vector2.up
+            || direction == Vector2.left || direction == Vector2.right);
 
         BoxCollider2D coll = GetComponent<BoxCollider2D>();
-        Vector3 newPos = transform.position + (direction * speed * Time.deltaTime);
+        Vector3 newPos = transform.position + (Vector3)(direction * speed * Time.deltaTime);
+
+        //// Compute points of rectangle
+        //// Rectangle encompasses area the hitbox will take up after the frame
+        //Vector2 pointA = new Vector2(coll.bounds.max.x, coll.bounds.min.y); // brCorner
+        //Vector2 blCorner = new Vector2(coll.bounds.min.x, coll.bounds.min.y);
+        //Vector2 pointB = blCorner + (direction * speed * Time.deltaTime);
+
+        //Debug.DrawLine(pointA, pointB);
+
+        //// Layer mask ensures only collidable objects are found in raycast
+        //int layerMask = 1 << 8; // bit sequence 1000 0000 - Only 'collidable' layer
+
+        //// Find any collisions via the rectangle
+        //// Collider with maximum y value is the one the body's collider will hug
+        //Collider2D[] colliders = Physics2D.OverlapAreaAll(pointA, pointB, layerMask);
+        //float maxY = Mathf.NegativeInfinity;
+        //BoxCollider2D maxYCollidable = null;
+        //foreach (BoxCollider2D collidable in colliders) {
+        //    if (collidable.bounds.max.y > maxY) {
+        //        maxY = collidable.bounds.max.y;
+        //        maxYCollidable = collidable;
+        //    }
+        //}
+
+        ////  if a collidable was found, update newPos to reflect the max y of the collidable
+        //if (maxYCollidable != null) {
+        //    newPos = new Vector3(newPos.x, maxYCollidable.bounds.max.y + coll.bounds.extents.y, newPos.z);
+        //}
+         
+        //// Update body's position
+        //transform.position = newPos;
 
         // Define offset to add to raycast, since raycast begins at origin of collider
         float offset;
-        if (direction == Vector3.left || direction == Vector3.right)
+        if (direction == Vector2.left || direction == Vector2.right)
         {
             offset = coll.size.x/2;
         }
@@ -39,28 +68,29 @@ public class Body : MonoBehaviour
         // Layer mask ensures only collidable objects are found in raycast
         int layerMask = 1 << 8; // bit sequence 1000 0000 - Only 'collidable' layer
 
-        // Cast raycast 
-        RaycastHit2D hit = 
-            Physics2D.Raycast(transform.position, direction, offset + speed * Time.deltaTime, layerMask);
-        Debug.DrawRay(transform.position, 
-            (direction * offset) +  (direction * speed * Time.deltaTime), Color.yellow);
+        // Cast boxcast 
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, coll.bounds.size, 
+            transform.eulerAngles.z, direction, speed * Time.deltaTime, layerMask);
+        ExtDebug.DrawBoxCastBox(transform.position, coll.bounds.extents, Quaternion.identity,
+            direction, speed * Time.deltaTime, Color.magenta);
+
 
         // Snap newPos to edge of any colliders found
         if (hit.collider != null)
         {
-            if (direction == Vector3.left)
+            if (direction == Vector2.left)
             {
                 newPos = new Vector3(hit.point.x + coll.size.x / 2, newPos.y, newPos.z);
             }
-            else if (direction == Vector3.right)
+            else if (direction == Vector2.right)
             {
                 newPos = new Vector3(hit.point.x - coll.size.x / 2, newPos.y, newPos.z);
             }
-            else if (direction == Vector3.down)
+            else if (direction == Vector2.down)
             {
                 newPos = new Vector3(newPos.x, hit.point.y + coll.size.y / 2, newPos.z);
             }
-            else if (direction == Vector3.up)
+            else if (direction == Vector2.up)
             {
                 newPos = new Vector3(newPos.x, hit.point.y - coll.size.y / 2, newPos.z);
             }
@@ -70,8 +100,7 @@ public class Body : MonoBehaviour
         transform.position = newPos;
     }
 
-    protected virtual void Update()
-    {
-        MoveBody(Vector3.down, gravity);
+    protected virtual void Update() {   
+        MoveBody(Vector2.down, gravity);
     }
 }
--- Platformer/Assets/Player.cs
+++ Platformer/Assets/Player.cs
@@ -14,27 +14,12 @@ public class Player : Body
         // Move player each frame if keys are held down
         if (Input.GetKey(KeyCode.D))
         {
-            MoveBody(Vector3.right, speed);
+            MoveBody(Vector2.right, speed);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            MoveBody(Vector3.left, speed);
+            MoveBody(Vector2.left, speed);
         }
-        
-        // Check if collider for this object is intersecting with collider for another object
-        // If it is, use the saved position of the gameObject at the beginning of the frame (i.e., in the update function for Body
-        // and revert to this position (save the position each update at the beginning in the Body script, then revert to this position)
-
-        // Checking should be at the very end of the update (before the position is updated in the next frame, so the sprites never intersect)
-
     }
-
-    //If your GameObject starts to collide with another GameObject with a Collider
-    void OnCollisionEnter(Collision collision)
-    {
-        //Output the Collider's GameObject's name
-        Debug.Log(collision.collider.name);
-    }
-
 }
