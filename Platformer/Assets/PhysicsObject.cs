b07798cc5d919a740a8a9fc974162fc343e749cd
Keeyan <kkazemi@sfu.ca>
2019-01-30 15:02:09 -0800

    Implemented collision detection
    Implemented the best version of collision detection I could after a lot of trial and error
    
    Issues: 2 consecutive blocks will stop the player from moving when their on the same y coordinate. To fix this, I made the colliders for platforms slightly smaller, added a buffer to the player for whenever he would be colliding with an object to ensure he's slightly floating above the collider.
    
    Also, added logic to tell whenever the player is grounded or not (to be used for jumping)

---
+++ Platformer/Assets/PhysicsObject.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+
+    // protected const float gravityScalar = 5;
+    public static float gravity = 0.1f;
+
+    protected bool grounded;
+    protected Rigidbody2D rb2d;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitResults = new RaycastHit2D[16];
+
+
+    void OnEnable() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Start() {
+        // Set up contact filter to include only collidable layer objects
+        //contactFilter.useTriggers = false; // Don't include triggers in results
+        contactFilter.SetLayerMask(1 << 8); // Include from collidable layer only
+        contactFilter.useLayerMask = true;
+    }
+
+    void FixedUpdate() {
+        // Simulate gravity
+        Move(Vector2.down, gravity);
+    }
+
+    // Move object speed units towards direction
+    // If there is a collidable in the way, object will not intersect it
+    protected void Move(Vector2 direction, float speed) {
+        Vector2 newPos = rb2d.position + (direction * speed);
+
+        // Default grounded to false, set to true if a downwards collision is found
+        if (direction == Vector2.down) { grounded = false; }
+
+        // Determine if there will be a collision
+        int count = rb2d.Cast(direction, contactFilter, hitResults, speed);
+
+        // Ensure there is a compatible collision before collision checking
+        bool compatibleCollisionFound = false;
+        for (int i = 0; i < count; i++) { 
+            if (hitResults[i].normal == -direction) {
+                compatibleCollisionFound = true;
+            }
+        }
+
+        // Determine how to update newPos
+        if (compatibleCollisionFound) {
+            Vector2 extents = GetComponent<BoxCollider2D>().bounds.extents;
+            float buffer = (float)0.01;
+            if (direction == Vector2.down) {
+
+                // Loop through hits to find the hit with the max or min point wrt direction
+                // Ignore any hits with an incompatible normal
+                float maxPointY = float.NegativeInfinity;
+                for (int i = 0; i < count; i++) {
+                    if (hitResults[i].normal == -direction) { // Only consider hits that are towards direction
+                        if (hitResults[i].point.y > maxPointY) {
+                            maxPointY = hitResults[i].point.y;
+                        }
+                    }
+                }
+                // Update newPos so that it isn't intersecting the other box collider
+                // Add a .001 buffer - Makes user slightly float above tiles, but prevents unwanted collisions
+                newPos = new Vector2(newPos.x, maxPointY + extents.y + buffer);
+                grounded = true; // User is now touching ground
+
+                // Reset gravity here
+            }
+
+            else if (direction == Vector2.up) {
+                float minPointY = float.PositiveInfinity;
+                for (int i = 0; i < count; i++) {
+                    if (hitResults[i].normal == -direction) {
+                        if (hitResults[i].point.y < minPointY) {
+                            minPointY = hitResults[i].point.y;
+                        }
+                    }
+                }
+                newPos = new Vector2(newPos.x, minPointY - extents.y - buffer);
+            }
+
+            else if (direction == Vector2.left) {
+                float maxPointX = float.NegativeInfinity;
+                for (int i = 0; i < count; i++) {
+                    if (hitResults[i].normal == -direction) { 
+                        if (hitResults[i].point.x > maxPointX) {
+                            maxPointX = hitResults[i].point.x;
+                        }
+                    }
+                }
+                newPos = new Vector2(maxPointX + extents.x + buffer, newPos.y);
+            }
+
+            else if (direction == Vector2.right) {
+                float minPointX = float.PositiveInfinity;
+                for (int i = 0; i < count; i++) {
+                    if (hitResults[i].normal == -direction) {
+                        if (hitResults[i].point.x < minPointX) {
+                            minPointX = hitResults[i].point.x;
+                        }
+                    }
+                }
+                newPos = new Vector2(minPointX - extents.x - buffer, newPos.y);
+            }
+        }
+
+        // Update rigidbody position
+        rb2d.position = newPos;
+    }
+}
--- Platformer/Assets/Player.cs
+++ Platformer/Assets/Player.cs
@@ -2,22 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Body
+public class Player : PhysicsObject
 {
-    // speed = 2
-    Player() : base(2) { }
-
-    protected override void Update()
-    {
-        base.Update();
-
-        // Move player each frame if keys are held down
-        if (Input.GetKey(KeyCode.D)) {
-            MoveBody(Vector2.right, speed);
-        }
-
-        if (Input.GetKey(KeyCode.A)) {
-            MoveBody(Vector2.left, speed);
-        }
-    }
+    //override void FixedUpdate()
+    //{
+    //    base.FixedUpdate();
+    //}
 }
